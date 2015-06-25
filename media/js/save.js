4aa7751692e56829ba5080884dbadf6ef59ceca6
Caleb Baze <cbaze19@gmail.com>
2015-06-24 23:07:40 -0500

    altered save functionality

--- media/js/save.js
+++ media/js/save.js
@@ -2,24 +2,19 @@ $(function() {
 
 	var socket = io();
 
-	var keys = {17: false, 16: false, 83: false};
-
-	$('#notepad').keydown(function(e) {
-	    if (e.keyCode in keys) {
-	        keys[e.keyCode] = true;
-	        if (keys[17] && keys[16] && keys[83]) {
-	        	var text = $('#notepad').val();
-	            socket.emit('update-text', {
-					'text' : text
-				});
-	            alert('Text Saved!');
-	        }
-	    }
-	}).keyup(function(e) {
-	    if (e.keyCode in keys) {
-	        keys[e.keyCode] = false;
-	    }
-	});
+	var map = []; // Or you could call it "key"
+	onkeydown = onkeyup = function(e){
+	    e = e || event; // to deal with IE
+	    map[e.keyCode] = e.type == 'keydown';
+	    if(map[17] && map[16] && map[83]){ // CTRL+SHIFT+A
+    		var text = $('#notepad').val();
+            socket.emit('update-text', {
+				'text' : text
+			});
+            alert('Text Saved!');
+            map = [];
+    	}
+	}
 
 	socket.on('update-text', function(data) {
 		$('#notepad').val(data);
